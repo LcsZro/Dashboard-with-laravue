e712ce6a19fe7e64c255fa6c1e378ad7ba6d43f0
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-09-09 10:48:37 -0300

    update

--- resources/js/Pages/Skills/Edit.vue
+++ resources/js/Pages/Skills/Edit.vue
@@ -63,7 +63,7 @@
                             @input="form.image = $event.target.files[0]"
                         />
 
-                        <InputError class="mt-2" :message="form.errors.image" />
+                        <InputError class="mt-2" :message="$page.props.errors.image" />
                     </div> 
 
                     <div class="flex items-center justify-end mt-4">
