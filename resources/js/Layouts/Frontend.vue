abfe3744098072328e22c7e969ae1b67ebd26c16
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-09-11 15:21:08 -0300

    update

---
+++ resources/js/Layouts/Frontend.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import Header from '@/Components/Frontend/Header.vue';
+</script>
+
+<template>
+    <div class="bg-slate-200 darkmode:bg-slate-900">
+        <!--Header-->
+        <Header/>
+        <main class="min-h-screen">
+            <slot/>
+        </main>
+        <!--Footer-->
+
+    </div>
+</template>
