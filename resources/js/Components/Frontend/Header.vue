d29ce5f2ad999f70715c7624534e1a2b5d9c961b
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-09-11 20:45:48 -0300

    update

--- resources/js/Components/Frontend/Header.vue
+++ resources/js/Components/Frontend/Header.vue
@@ -1,16 +1,13 @@
 <template> 
     <nav class="flex items-center justify-between flex-wrap bg-[#242424] p-6">
-            <!-- Logo à esquerda -->
             <div class="flex items-center flex-shrink-0 text-white mr-6">
                 <h2 class="font-semibold text-xl tracking-tight text-[#69B42D] dark:text-white">Portfólio</h2>
             </div>
-            <!-- Botão do menu à direita -->
             <div class="block lg:hidden">
                 <button class="flex items-center px-3 py-2 border rounded text-xl text-white border-[#68B42D] hover:text-white hover:border-white" @click="toggleMobileMenu">
                     <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                 </button>
             </div>
-            <!-- Itens de menu -->
             <div class="w-full lg:flex lg:items-center lg:w-auto" :class="{ 'hidden': !showMobileMenu }">
                 <div class="text-lg lg:flex-grow text-[#68B42D]">
                 <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4">
