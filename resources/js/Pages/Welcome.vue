f1090f45cedc18db401a25a47d5b374abbc61752
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-09-13 15:29:43 -0300

    update

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,14 +2,19 @@
 import { Head, Link } from '@inertiajs/vue3';
 import FrontendLayout from '@/Layouts/Frontend.vue';
 import Hero from '@/Components/Frontend/Hero.vue';
-import About from '@/Components/Frontend/About.vue';
+import Skills from '@/Components/Frontend/Skills.vue';
+
+defineProps({
+    skills: Object,
+})
 </script>
 
 <template>
     <Head title="Welcome to portfolio" />
     <FrontendLayout>
         <Hero/>
-        <About/>
+        <About/> 
+        <Skills :skills="skills" />
     </FrontendLayout>
 </template>
 
