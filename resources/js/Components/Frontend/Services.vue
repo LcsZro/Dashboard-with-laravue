9aee97a2e398ea75d6ec3a2bbe91fdef2da43bcc
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-10-02 15:54:33 -0300

    update

--- resources/js/Components/Frontend/Services.vue
+++ resources/js/Components/Frontend/Services.vue
@@ -17,7 +17,7 @@
                     <img class="w-10 " src="../Frontend/Img/responsive.svg" alt="responsive">
                 </div>              
                 <div class="">
-                    <h4 class="text-xl font-medium mb-2 text-center">Web Design</h4>
+                    <h4 class="text-xl font-medium mb-2 text-center">Responsive Design</h4>
                     <p class="text-[#2F2E41] dark:text-white text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, ipsum quae totam libero accusantium iste illum excepturi culpa voluptas iusto doloribus debitis temporibus sit nobis minus!</p>
                 </div>
             </div>
@@ -26,7 +26,7 @@
                     <img class="w-10 " src="../Frontend/Img/laptop.svg" alt="frontend">  
                 </div>              
                 <div class="">
-                    <h4 class="text-xl font-medium mb-2  text-center">Web Design</h4>
+                    <h4 class="text-xl font-medium mb-2  text-center">Web Development</h4>
                     <p class="text-[#2F2E41] dark:text-white text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, ipsum quae totam libero accusantium iste illum excepturi culpa voluptas iusto doloribus debitis temporibus sit nobis minus!</p>
                 </div>
             </div>
@@ -41,10 +41,10 @@
             </div>
               <div class="p-6 bg-gray-950-800 rounded-2xl border-2 border-t-lime-500">
                 <div class="flex items-center justify-center">
-                    <img class="w-10 " src="../Frontend/Img/seo-line.svg" alt="SEO">  
+                    <img class="w-10" src="../Frontend/Img/seo-line.svg" alt="SEO">  
                 </div>              
                 <div class="">
-                    <h4 class="text-xl font-medium mb-2 text-center">Web Design</h4>
+                    <h4 class="text-xl font-medium mb-2 text-center">SEO</h4>
                     <p class="text-[#2F2E41] dark:text-white text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, ipsum quae totam libero accusantium iste illum excepturi culpa voluptas iusto doloribus debitis temporibus sit nobis minus!</p>
                 </div>
             </div>
