5f6c4dee503dc35b02f8bdb529915145c77c9adb
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-10-02 14:25:53 -0300

    update

--- resources/js/Components/Frontend/Project.vue
+++ resources/js/Components/Frontend/Project.vue
@@ -26,7 +26,7 @@
           </p>
         </div>
         <div class="flex flex-wrap gap-2">
-          <span class="badge info bg-slate-300 dark:bg-white" v-for="(skill, index) in project.selectedSkills" :key="index">
+          <span class="badge bg-zinc-700 dark:bg-[#1B1B1B]" v-for="(skill, index) in project.selectedSkills" :key="index">
             <p :style="{ color: project.color[index] }">{{ skill }}</p>
           </span>
         </div>
