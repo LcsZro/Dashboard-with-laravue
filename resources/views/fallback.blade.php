102d3d48eb86bc281e71ef2392f2889f70bd9f70
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-10-02 20:50:13 -0300

    update

--- resources/views/fallback.blade.php
+++ resources/views/fallback.blade.php
@@ -7,20 +7,16 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    
-    <html class="">
-        <div class="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
-            <div class="text-center">
-                <p class="text-base font-semibold text-[#68B42D]">404</p>
-                <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
-                <p class="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
-                <div class="mt-10 flex items-center justify-center gap-x-6">
-                    <a href="/" class="rounded-md bg-lime-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-lime-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-400">Go back home</a>
-                    <a href="#" class="text-sm font-semibold text-gray-900 hover:text-lime-600">Contact support <span aria-hidden="true">&rarr;</span></a>
-                </div>
+    <div class="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+        <div class="text-center">
+            <p class="text-base font-semibold text-[#68B42D]">404</p>
+            <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
+            <p class="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+            <div class="mt-10 flex items-center justify-center gap-x-6">
+                <a href="/" class="rounded-md bg-lime-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-lime-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-400">Go back home</a>
+                <a href="#" class="text-sm font-semibold text-gray-900 hover:text-lime-600">Contact support <span aria-hidden="true">&rarr;</span></a>
             </div>
         </div>
-    </html>
-  
+    </div>
 </body>
 </html>
