3080550c0fd45e60e354fad10acb4e2cda1fde50
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-10-03 17:00:23 -0300

    update SetContentTypeHeader

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
             \App\Http\Middleware\TrustProxies::class,
+            \App\Http\Middleware\SetContentTypeHeader::class,
         ],
 
         'api' => [
