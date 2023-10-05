c6015f37e063c596fddd4301dafa9b744f4d2491
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-10-05 17:23:54 -0300

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -16,6 +15,6 @@
         @inertiaHead
     </head>
     <body class="!font-body">
-        @inertia
+        @inertia    
     </body>
 </html>
