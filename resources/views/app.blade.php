6c23c4a375babf54f83d685e456fe54fe842bffd
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-10-05 15:59:16 -0300

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self'; report-uri /endpoint-de-relatórios-csp;">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
