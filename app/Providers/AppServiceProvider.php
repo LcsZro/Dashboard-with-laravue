22e7b647795ee41572052ba9508a4f4e01fa0b5b
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-10-06 14:12:25 -0300

    Revert "update"
    This reverts commit 9eafae34cff585613748a3a092d987fbb9dd20e1.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_FORCE_HTTPS', false)) {
-            URL::forceScheme('https');
-        }
+       
     }
 }
