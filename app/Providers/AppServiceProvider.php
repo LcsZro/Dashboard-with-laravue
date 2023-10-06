9eafae34cff585613748a3a092d987fbb9dd20e1
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-10-06 14:07:17 -0300

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-       
+        if (env('APP_FORCE_HTTPS', false)) {
+            URL::forceScheme('https');
+        }
     }
 }
