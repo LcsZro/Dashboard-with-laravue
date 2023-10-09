b4f83d524e617f0025b2a093dc67647d06f18a1e
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-10-09 18:52:08 -0300

    up

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-        
+        <link rel="stylesheet" href="https://portfolio-production-f53d.up.railway.app/build/assets/app-896edaca.css">
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
