ac2c07ed53c8f78e02deafbe6d4c01cd16c9e884
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-10-09 15:18:47 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,16 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: 'resources/js/app.js', // O caminho para o ponto de entrada do seu aplicativo Vue.js
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        vue(),
     ],
     build: {
-        outDir: 'public/build',
+        outDir: 'public/build', // O diretório onde os ativos compilados serão gerados
+    },
+    server: {
+        host: 'localhost', // O host onde o servidor de desenvolvimento do Vite será executado
+        port: 3000, // A porta para o servidor de desenvolvimento do Vite
     },
 });
