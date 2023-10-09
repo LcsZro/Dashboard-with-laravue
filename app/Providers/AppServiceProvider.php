9d3a9c824fdc95b81c470db67fe96ea701858a4e
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-10-09 18:36:41 -0300

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(env(key: 'APP_ENV') !== 'local') {
-            URL::forcescheme(scheme: 'https');
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
         }
     }
 }
