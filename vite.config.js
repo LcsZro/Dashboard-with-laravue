306c13f50fc6a05dda6bb2f6670a6427329b82af
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-10-09 21:58:31 -0300

    up

--- vite.config.js
+++ vite.config.js
@@ -3,30 +3,23 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      // Set the correct entry point for your application
-      input: 'resources/js/app.js',
-      refresh: true,
-    }),
-    vue({
-      template: {
-        transformAssetUrls: {
-          base: null,
-          includeAbsolute: false,
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }), 
+    ],
+    build: {
+        output: {
+            manifest: 'public/build/manifest.json',
         },
-      },
-    }),
-  ],
-  build: {
-    // Set the correct output directory for your built assets
-    outDir: 'public/build',
-    // Specify the name of the entry point (if different from 'app')
-    rollupOptions: {
-      input: {
-        app: 'resources/js/app.js',
-        // Add any additional entry points if needed
-      },
     },
-  },
 });
