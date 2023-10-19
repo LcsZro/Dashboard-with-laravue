bbc83085872654b98960ec56fb97bdb5cc289f8f
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-10-19 13:03:12 -0300

    update filesystem upload

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => '/storage', 
+            'url' => env('APP_URL').'/public/uploads',
             'visibility' => 'public',
             'throw' => false,
         ],
